467a791dafa6592cf1261d3800e18d0e2dad758a
Cristian <cristian.coppari@gmail.com>
2024-11-27 15:07:30 -0300

    feature: add footer with the menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import Navbar from "~/components/layout/navbar/navbar";
+import Footer from "~/components/layout/footer";
 import "~/app/globals.css";
 
 const geistSans = localFont({
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import { getMenu } from "~/lib/shopify";
+
+export default async function Footer() {
+  const menu = await getMenu("nextjs-footer-menu");
+  console.log({ menu });
+  return (
+    <footer className="bg-gray-900">
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <nav aria-label="Footer" className="-mb-6 flex flex-wrap justify-center gap-x-12 gap-y-3 text-sm/6">
+          {menu.map((item) => (
+            <Link key={item.title} href={item.path} className="text-gray-400 hover:text-white">
+              {item.title}
+            </Link>
+          ))}
+        </nav>
+        <p className="mt-10 text-center text-sm/6 text-gray-400">&copy; 2024 Desarrollador Web.</p>
+      </div>
+    </footer>
+  );
+}
